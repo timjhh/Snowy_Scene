6fa8f2699bcf6ce02c21d1db1081604f5c8896e4
Tim Harrold <timjharrold@gmail.com>
2021-10-28 21:26:00 -0400

    Stars!
    - Added randomly generated stars
    - Fixed geometry on plane,camera
    - Attempting to add moon!

--- src/App.js
+++ src/App.js
@@ -31,70 +31,66 @@ function initScene() {
   //scene.background = new THREE.Color( 0x222244 );
   scene.add( new THREE.AmbientLight( 0x444444 ) );
   //scene.fog = new THREE.FogExp2(0xffffff)
-  scene.fog = new THREE.Fog( 0x000000, 4000, 8000 ); 
+  //scene.fog = new THREE.Fog( 0x000000, 4000, 8000 ); 
 
   // Create snowy field plane
-  let planeGeom = new THREE.PlaneGeometry(10000,10000);
+  let planeGeom = new THREE.PlaneGeometry(5000,5000);
   let planeMesh = new THREE.MeshBasicMaterial( {color: 0xc2bfb8, side : THREE.DoubleSide })
   var plane = new THREE.Mesh(planeGeom, planeMesh);
 
   // Rotate to viewable angle
   plane.rotation.x = Math.PI / 2;
 
-
-
-
   scene.add(plane);
 
 
-  const stars = [];
-  let numstars = 500;
-  for(var i = 0; i < numstars; i++) {
 
-    const x = THREE.MathUtils.randFloatSpread(2000);
-    const y = THREE.MathUtils.randFloatSpread(2000);
-    const z = THREE.MathUtils.randFloatSpread(2000);
+  let numstars = 500;
+
+
+  // After rendering past the bounds of our scene, multiply by a conditional -1
+  // To apply the stars symmetrically
+  var neg = 1;
+
+  // Minimum distance star can take
+  var min = 0;
+
+  // Maximum distance star can take
+  var max = 10000;
+
+  var range = 4;
+
+  // GENERATE A RANDOM # OF STARS
+  for(var i = 0; i < 4000; i++) {
+
+
+
+    let starGeom = new THREE.BoxGeometry(1, 1, 1);
+    let starMat = new THREE.MeshBasicMaterial({color: 0xffffff});
+    let star = new THREE.Mesh(starGeom, starMat);
+
     
-    stars.push(x,y,z);
+    star.position.x = ((max/2) - (Math.random() * max)) * range;
+
+    // Only positive values allowed
+    star.position.y = (Math.random() * max);
+    
+
+    star.position.z = ((max/2) - (Math.random() * max)) * range;
+    
+
+    star.scale.x = star.scale.y = 8;
+
+
+    scene.add(star);
+
+    // Apply the next star across the scene
+    neg *= -1;
 
   }
 
-  const starGeom = new THREE.BufferGeometry();
-  starGeom.setAttribute('position', new THREE.Float32BufferAttribute(stars, 3));
+  let moonGeom = new THREE.SphereGeometry(200, 100, 100)
 
-  const starMat = new THREE.PointsMaterial({ color: 0x888888 });
-
-  const points = new THREE.Points(starGeom, starMat);
-
-  scene.add(stars);
-
-
-  // // Manually load in skybox images
-  // const ft = new THREE.TextureLoader().load("/skybox/GTX_ft.png");
-  // const bk = new THREE.TextureLoader().load("/skybox/GTX_bk.png");
-  // const up = new THREE.TextureLoader().load("/skybox/GTX_up.png");
-  // const dn = new THREE.TextureLoader().load("/skybox/GTX_dn.png");
-  // const rt = new THREE.TextureLoader().load("/skybox/GTX_rt.png");
-  // const lf = new THREE.TextureLoader().load("/skybox/GTX_lf.png");
-
-  // // Create skybox
-  // let skyGeom = new THREE.BoxGeometry(10000, 10000, 10000);
-  // let textures = [ft,bk,up,dn,lf,rt];
-  // let materials = [];
-
-  // textures.forEach(d => {
-
-  //     materials.push(new THREE.MeshBasicMaterial({ map: d, side: THREE.BackSide  }));
-
-  // });
-
-  // var skyMaterial = new THREE.MeshFaceMaterial(materials);
-  // skybox = new THREE.Mesh(skyGeom, materials);
-
-  scene.add(skybox);
-  // skybox.position.x = 0;
-  // skybox.position.y = 1000;
-  // skybox.position.z = 0;
 
 }
   
@@ -102,7 +98,7 @@ function initScene() {
 
     const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 20000 );
 
-    camera.position.set(0,1000,5000);
+    camera.position.set(0,5000,0);
     camera.lookAt(0,0,0);
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
