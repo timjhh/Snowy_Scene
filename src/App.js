7d5d0a829610d69ab449daa9a9e6a93ff703b3b2
Tim Harrold <timjharrold@gmail.com>
2021-10-28 23:08:39 -0400

    Ambiance, Layout
    - Updated gitignore
    - Added many trees to layout
    - Added fog to scene

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ componentDidMount() {
 
 let scene;
 let skybox;
+var planeLen = 8000;
 var stars = [];
 var snows = [];
 
@@ -29,14 +30,16 @@ function initScene() {
 
   scene = new THREE.Scene();
   //scene.background = new THREE.Color( 0x222244 );
+  scene.background = new THREE.Color( 0x222244 );
   //scene.add( new THREE.AmbientLight( 0x444444 ) );
   //scene.fog = new THREE.FogExp2(0xffffff)
-  //scene.fog = new THREE.Fog( 0x000000, 4000, 8000 ); 
+  scene.fog = new THREE.Fog( 0x222244, (100), (planeLen/2) ); 
 
   // Create snowy field plane
-  let planeGeom = new THREE.PlaneGeometry(5000,5000);
+  let planeGeom = new THREE.PlaneGeometry(planeLen,planeLen);
   //let planeMesh = new THREE.MeshBasicMaterial( {color: 0xc2bfb8, side : THREE.DoubleSide })
-  let planeMesh = new THREE.MeshPhongMaterial({color: '#8AC', side : THREE.DoubleSide});
+  //let planeMesh = new THREE.MeshPhongMaterial({color: '#8AC', side : THREE.DoubleSide});
+  let planeMesh = new THREE.MeshLambertMaterial({color: '#8AC', side : THREE.DoubleSide, reflectivity: 0.5});
 
   var plane = new THREE.Mesh(planeGeom, planeMesh);
 
@@ -50,7 +53,7 @@ function initScene() {
 
   let numStars = 4000;
   let numSnow = 1000;
-
+  let numTrees = 30;
 
   // Maximum distance star can take
   var max = 10000;
@@ -93,18 +96,19 @@ function initScene() {
   //let moonGeom = new THREE.SphereGeometry(400, 100, 100);
   let moonGeom = new THREE.IcosahedronGeometry(400, 0);
   let moonMat = new THREE.MeshBasicMaterial({color: 0xF4F6F0});
+  //let moonMat = new THREE.MeshPhongMaterial({color: 0xF4F6F0});
   let moon = new THREE.Mesh(moonGeom, moonMat);
 
   let color = "0xffffff";
   let intensity = 1;
 
   let moonLight = new THREE.DirectionalLight(color,intensity);
-  moonLight.position.set(5000,5000,5000);
+  moonLight.position.set(planeLen,planeLen,planeLen);
   moonLight.target.position.set(0,0,0);
 
-  moon.position.x = 5000;
-  moon.position.y = 5000;
-  moon.position.z = 5000;
+  moon.position.x = planeLen;
+  moon.position.y = planeLen;
+  moon.position.z = planeLen;
 
   scene.add(moon);
   scene.add(moonLight);
@@ -132,26 +136,43 @@ function initScene() {
   }
 
 
+  // Create a pretty pine tree :)
+  function genTree(x, z, size, leaves) {
+
+    const tree = new THREE.Group();
+
+    for(var i = leaves; i >= 1; i--) {
+
+      let leafGeom = new THREE.OctahedronGeometry(i*(size/2), 0);
+      let leafMat = new THREE.MeshLambertMaterial({color: "#015045"});
+
+      let leaf = new THREE.Mesh(leafGeom, leafMat);
+
+      leaf.position.x = x;
+      leaf.position.y = ((i/leaves) * size) + ((leaves-i) * size) - size/2;
+      leaf.position.z = z;
+
+
+      tree.add(leaf);
+    }
+
+    return tree;
+  }
+
   // Leaf color: #015045
   let treeBase = new THREE.CylinderGeometry(5, 80, 2000, 32);
   let treeMat = new THREE.MeshBasicMaterial( {color: "#3b3429", side : THREE.DoubleSide });
   //let treeMat = new THREE.MeshPhongMaterial({color: "#015045", side : THREE.DoubleSide});
   let tree = new THREE.Mesh(treeBase, treeMat);
 
-  let numLeaves = 4;
-  for(var i = numLeaves; i >= 1; i--) {
-    let leafGeom = new THREE.OctahedronGeometry(i*50, 0);
-    let leafMat = new THREE.MeshLambertMaterial({color: "#015045"});
-
-    let leaf = new THREE.Mesh(leafGeom, leafMat);
-
-    leaf.position.x = 500;
-    leaf.position.y = ((i/numLeaves) * 100) + ((numLeaves-i) * 100);
-    leaf.position.z = 300;
 
 
-    scene.add(leaf);
+  for(var i = 0; i < numTrees; i++) {
+    let x = (planeLen/2) - (Math.random() * planeLen);
+    let z = (planeLen/2) - (Math.random() * planeLen);
+    scene.add(genTree(x, z, 100, 5));    
   }
+
 
   tree.position.x = 500;
   tree.position.z = 300;
@@ -176,8 +197,8 @@ function initScene() {
 
     const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 20000 );
 
-    camera.position.set(0,5000,0);
-    camera.lookAt(5000,5000,5000);
+
+
 
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -195,13 +216,14 @@ function initScene() {
     controls.maxDistance = 200.0;
     controls.update();
 
-
+    camera.position.set(0,200,0);
+    camera.lookAt(5000,3000,5000);
 
     const clock = new THREE.Clock();
 
 
 
-    camera.position.z = 15;
+    
 
     initScene();
 
